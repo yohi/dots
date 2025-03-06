915ea8f26e6e8959cdf63ac846cd32e31e5375e1
Yusuke Ohi <y.ohi@diamondhead.tech>
2025-03-06 16:28:23 +0900

    nvim-cmp keymap

--- vim/lua/plugins/nvim-cmp.lua
+++ vim/lua/plugins/nvim-cmp.lua
@@ -36,9 +36,8 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
+                ['<C-j>'] = cmp.mapping.select_next_item(),
+                ['<C-k>'] = cmp.mapping.select_prev_item(),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
             }),
