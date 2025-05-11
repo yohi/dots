61d57276f362ba90333f25760083e32b96620ec8
Yusuke Ohi <yohi.consadole12@gmail.com>
2025-05-11 10:36:31 +0900

    更新

--- vim/lua/lazy.lua
+++ vim/lua/lazy.lua
@@ -1,6 +1,6 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-print("lazypath: " .. lazypath)
+-- print("lazypath: " .. lazypath)
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
